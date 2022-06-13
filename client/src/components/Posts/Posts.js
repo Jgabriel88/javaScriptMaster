e4f2dceab5c9b998d375ff0cee922f846398e547
Jgabriel88 <j.gabriel.888@gmail.com>
2022-06-13 19:39:58 -0400

    firt post render

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () =>{
+   return( <>
+    <h1>Posts</h1>
+    <Post></Post>
+    <Post></Post>
+    <h1>TEST</h1>
+    </>
+   )
+}
+
+
+export default Posts 
